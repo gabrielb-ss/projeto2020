9cb16dc143575a5826086fc55f680f51cc74e541
Gabriel Barbosa <1stmopt@gmail.com>
2020-02-13 18:46:57 -0200

    mira inversa e limites de mira implementados

--- Arrow.gd
+++ Arrow.gd
@@ -3,5 +3,5 @@ extends RigidBody2D
 var speed = 350
 
 func _ready():
-	apply_impulse(Vector2(), Vector2(speed, 0).rotated(rotation))
-
+#	apply_impulse(Vector2(), Vector2(speed, 0).rotated(rotation))
+	pass
