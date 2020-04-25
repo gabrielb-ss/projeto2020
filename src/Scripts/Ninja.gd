760181dce6914f6c3005a9711206d6d8a81514fb
Gabriel B <1stmopt@gmail.com>
2020-04-25 15:18:36 -0300

    Hunter renomeado para Ninja; Mecanicas de pulo carregado e pulo duplo implementadas

--- src/Scripts/Ninja.gd
+++ src/Scripts/Ninja.gd
@@ -3,15 +3,17 @@ extends KinematicBody2D
 const FLOOR =  Vector2(0, -1)
 const SPEED_LIMIT = 300
 const GRAVITY = 10
-const JUMP = -200
+const JUMP_LIMIT = -400
 
 var velocity = Vector2()
 var on_ground = false
 var curr_speed = 0
+var curr_jump = -200
 var accel = 2
 var last_side = 0
 var attacking = false
 var curr_anim = ""
+var double_jump = true
 
 func _ready():
 	$Timer.connect("timeout", self, "atk_off")
@@ -89,13 +91,24 @@ func get_input(delta):
 			last_side = 0
 	
 	if Input.is_action_pressed("ui_up"):
-		if on_ground:
-			velocity.y = JUMP
-			velocity.x += curr_speed/2 * last_side
+		if curr_jump > JUMP_LIMIT and on_ground:
+			curr_jump -= 4
+	
+	elif Input.is_action_just_released("ui_up") and double_jump:
+			double_jump = false
+			velocity.y = curr_jump
+			
+			if abs(curr_speed) < 100:
+				velocity.x += 200 * last_side
+			else:
+				velocity.x += curr_speed/2 * last_side 
+			
 			on_ground = false
+			curr_jump = -200
 			
 	if is_on_floor():
 		on_ground = true
+		double_jump = true
 	else:
 		on_ground = false
 		if velocity.y < 0:
@@ -111,6 +124,7 @@ func get_input(delta):
 		
 func try_move(rel_vec):
 	if test_move(transform,rel_vec):
+		print("cant")
 		curr_speed = 0
 		return false
 	else:
