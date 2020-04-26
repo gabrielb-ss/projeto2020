5f420c5c5b9805fbc4dea444fc2a33be19e74b52
Gabriel Barbosa <1stmopt@gmail.com>
2020-04-26 02:25:00 -0300

    inimigo agora persegue o jogador; falta animar, implementar aceleração e dano

--- src/Scripts/Enemy.gd
+++ src/Scripts/Enemy.gd
@@ -1,39 +1,13 @@
 extends KinematicBody2D
 
-const FLOOR =  Vector2(0, -1)
 const GRAVITY = 10
-var speed = -100
-var velocity = Vector2(-75,0)
-var on_ground = false
-var hp = 200
-
+const FLOOR =  Vector2(0, -1)
+var velocity = Vector2()
+var speed = 150
+onready var obj = get_parent().get_node("Ninja")
 
 func _physics_process(delta):
-	if is_on_floor():
-		on_ground = true
-	else:
-		on_ground = false
-	
-	velocity = move_and_slide(velocity, FLOOR)
+	var dir = (obj.global_position - global_position).normalized()
+	velocity.x = dir.x * speed
 	velocity.y += GRAVITY
-	velocity.x = speed
-#	if u_turn():
-		
-	$Sprite.play("Walk")
-	
-	
-func _on_Area2D_body_entered(body):
-	var name = " " 
-	name = body.get_name()
-	if  name == "Enviroment":
-		apply_scale(Vector2(-1,1))
-		speed *= -1
-	elif name == "Player":
-		velocity.x *= 2
-	elif name.find("Arrow", 0) != -1:
-		var currhp = $Hp.get_value()
-		currhp -= 25
-		$Hp.set_value(currhp)
-		
-		if currhp <= 0:
-			queue_free()
+	velocity = move_and_slide(velocity, FLOOR)
